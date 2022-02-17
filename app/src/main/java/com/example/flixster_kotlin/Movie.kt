784761140abb648b29972e71483a3de57381e359
Shuhuasong <shuhuasong888@gmail.com>
2022-02-17 00:03:58 -0500

    Detail Screem Implementation

--- app/src/main/java/com/example/flixster_kotlin/Movie.kt
+++ app/src/main/java/com/example/flixster_kotlin/Movie.kt
@@ -1,16 +1,23 @@
 package com.example.flixster_kotlin
 
+import android.os.Parcelable
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie(
         val movieId: Int,
+        val voteAverage: Double,
         private val posterPath: String,
         private val backdropPath: String,
         val title: String,
         val overview: String,
-){
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     val backdropImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
+
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray?): List<Movie> {
             val movies = mutableListOf<Movie>()
@@ -20,6 +27,7 @@ data class Movie(
                     movies.add(
                        Movie(
                            movieJson.getInt("id"),
+                           movieJson.getDouble("vote_average"),
                            movieJson.getString("poster_path"),
                            movieJson.getString("backdrop_path"),
                            movieJson.getString("title"),
--- app/src/main/java/com/example/flixster_kotlin/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster_kotlin/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster_kotlin
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,8 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) :
@@ -35,13 +39,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     //get the reference of each component in the itemView, which are one imageView, and two textView
     //and populate the data for each component by using current movie data
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
 
-        fun bind(movie: Movie) {
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             var imageUrl = movie.posterImageUrl
@@ -50,6 +57,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 imageUrl = movie.backdropImageUrl
             }
             Glide.with(context).load(imageUrl).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+           //1. Get notified of a particular move which was clicked
+            val movie = movies[adapterPosition]
+            //Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+           //2. Use the intent system to navigate to te new activity
+            val intent = Intent(context, DetailActivity::class.java)
+             intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
